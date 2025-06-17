3e62b06e6ef65e3677bae91be368ad81b7cce239
lotannauba <lotannauba23@gmail.com>
2025-06-17 17:40:07 +0100

    Add files via upload

---
+++ TaskFour.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class TaskTwo{
+	public static void main(String[]args){
+		Scanner lotanna = new Scanner(System.in);
+
+
+System.out.print("Enter an integer of your choice : ");
+		int Multiple = lotanna.nextInt();
+	
+
+
+	Multiple = 0;
+
+	if(Multiple % 4 == 0){
+		System.out.print(Multiple);
+	
+	}else{
+		System.out.print("This is not a Multiple");
+	
+		}
+
+
+
+
+
+
+}
+
+}
