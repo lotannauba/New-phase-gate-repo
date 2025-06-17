dcb9eb0abd32fc2e6379742c7ff1ab85698e6412
lotannauba <lotannauba23@gmail.com>
2025-06-17 17:40:30 +0100

    Add files via upload

---
+++ TaskFive.java
@@ -0,0 +1,9 @@
+public class TaskFive{
+	public static void main(String[]args){
+	
+for int count = 4; count <= 8; count+= 4{
+	for counter = 1; counter <=5; counter++{
+System.out.print(counter);
+}
+}
+
